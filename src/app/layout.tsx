8a0ea9f23c4053729ac07c37b215a5c485016a1d
ndaemy <yuyaebean@gmail.com>
2023-11-29 02:48:11 +0900

    feat: 기본 layout 잡기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,14 @@ export default function RootLayout({
     <html lang="ko">
       <body className="font-default">
         <Header />
-        {children}
+        <main
+          className="relative min-h-screen
+            bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900"
+        >
+          <div className="px-6 pt-20 mx-auto max-w-7xl lg:px-8 md:pt-24 lg:pt-32">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
