e2b87bef8966c5f27144d3d294fa1a5dac1a961e
ndaemy <yuyaebean@gmail.com>
2024-10-09 23:33:13 +0900

    feat: 포트폴리오 상단 부분 내용 채우기

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+const user = {
+  name: "유예빈",
+  jobTitle: "서비스 개발자",
+  email: "yuyaebean@gmail.com",
+  avatar: "/images/avatar.jpg",
+  bio: "서비스를 직접 만드는 것이 재미있어서 처음부터 끝까지 혼자서 만들 수 있는 기술을 가지기 위해 노력하는 개발자입니다. 열정 있는 사람들과 뭔가 하는 것에도 흥미가 있어 이런저런 활동도 많이 하는 사람입니다.",
+};
+
+export { user };
