94073df8bb228ac2ec6391a324df509a30699d71
ndaemy <yuyaebean@gmail.com>
2024-10-10 04:50:35 +0900

    feat: Pretendard 폰트 적용 및 기존 폰트 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "@/app/ui/globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  variable: "--font-pretendard",
+  weight: "45 920",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${pretendard.variable} antialiased`}>{children}</body>
     </html>
   );
 }
