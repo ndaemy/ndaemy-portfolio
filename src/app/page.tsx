bf6c93a6313f97fc67ce1a231d9a9fccac6dd900
ndaemy <yuyaebean@gmail.com>
2024-10-13 04:52:01 +0900

    feat: 포트폴리오 상단 부분 기본 디자인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,63 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
 import { user } from "@/config";
 
 export default function Home() {
   return (
-    <>
-      <h1 id="name">{user.name}</h1>
-      <h4 id="job-title">{user.jobTitle}</h4>
-      <a href={`mailto:${user.email}`}>
-        <button>Email</button>
-      </a>
-      <Image src={user.avatar} alt="avatar" width={200} height={200} />
-      <div id="bio">{user.bio}</div>
-    </>
+    <main>
+      <div className="flex justify-center pt-20">
+        <section className="mx-10 flex w-full max-w-screen-lg flex-col gap-4 md:mx-14">
+          <div className="metadata flex w-full justify-between">
+            <div>
+              <div>
+                <h1
+                  id="name"
+                  className="text-4xl font-bold leading-normal text-primary"
+                >
+                  {user.name}
+                </h1>
+                <h4 id="job-title" className="prose-2xl">
+                  {user.jobTitle}
+                </h4>
+              </div>
+              <div className="divider divider-neutral w-32" />
+              <div id="links" className="flex items-center gap-4">
+                <a href={`mailto:${user.email}`}>
+                  <button className="btn btn-link btn-lg px-0">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    Email
+                  </button>
+                </a>
+                <span>/</span>
+                <a
+                  href={`https://github.com/${user.githubUsername}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="btn btn-link btn-lg px-0">
+                    <FontAwesomeIcon icon={faGithub} />
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+            <Image
+              id="avatar"
+              src={user.avatar}
+              alt="avatar"
+              className="h-40 w-40 rounded-full"
+              width={160}
+              height={160}
+            />
+          </div>
+          <div id="bio" className="prose prose-xl max-w-full">
+            {user.bio}
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
