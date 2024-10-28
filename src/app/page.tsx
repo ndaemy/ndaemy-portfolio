63c7771033635ce3a03c83b564ece77916f946cc
ndaemy <yuyaebean@gmail.com>
2024-10-28 19:28:57 +0900

    feat: 소소한 디자인 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
   return (
     <main>
       <div className="mx-10 flex flex-col items-center gap-12 pt-20 *:w-full *:max-w-screen-lg md:mx-14">
-        <section id="portfolio-head" className="flex w-full flex-col gap-4">
+        <section id="portfolio-head" className="flex w-full flex-col gap-2">
           <div id="metadata" className="flex justify-between">
             <div>
               <div>
                 <h1
                   id="name"
-                  className="text-4xl font-bold leading-normal text-primary"
+                  className="mb-3 text-4xl font-bold leading-normal text-primary"
                 >
                   {user.name}
                 </h1>
@@ -31,7 +31,7 @@ export default function Home() {
               <div className="divider divider-neutral w-32" />
               <div id="links" className="flex items-center gap-4">
                 <a href={`mailto:${user.email}`}>
-                  <button className="btn btn-link btn-lg px-0">
+                  <button className="btn btn-link btn-lg h-fit min-h-0 px-0 py-4">
                     <FontAwesomeIcon icon={faEnvelope} />
                     Email
                   </button>
@@ -42,7 +42,7 @@ export default function Home() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <button className="btn btn-link btn-lg px-0">
+                  <button className="btn btn-link btn-lg h-fit min-h-0 px-0 py-4">
                     <FontAwesomeIcon icon={faGithub} />
                     GitHub
                   </button>
@@ -66,7 +66,7 @@ export default function Home() {
           <div className="flex flex-col gap-12">
             <div className="flex flex-col gap-2.5">
               <h2 className="prose prose-2xl font-bold">Skills</h2>
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-4">
                 {Object.keys(user.skills).map(key => (
                   <BadgesWithTitle
                     key={key}
