7cfb737077f7cf756f3c957ca0fd4f603e381253
ndaemy <yuyaebean@gmail.com>
2024-10-30 18:51:55 +0900

    chore: tailwind.config.ts 파일 불필요한 코드 정리

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
+import typography from "@tailwindcss/typography";
 import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
-import typography from "@tailwindcss/typography";
 
 const config: Config = {
   content: [
@@ -9,12 +9,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [typography, daisyui],
   daisyui: {
