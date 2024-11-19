734cd0189653687e56b12e7a727c661cf6193cdb
ndaemy <yuyaebean@gmail.com>
2024-11-19 17:32:51 +0900

    feat: Activities 와 Careers 섹션 모바일 대응

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -11,10 +11,10 @@ export default function Activities({ activities }: ActivitiesProps) {
     <ul className="prose flex max-w-none flex-col gap-4">
       {activities.map(activity => (
         <li key={activity.slug}>
-          <div className="grid grid-cols-[200px_minmax(0,_1fr)] gap-4">
+          <div className="grid grid-cols-1 gap-2 sm:grid-cols-[200px_minmax(0,_1fr)] sm:gap-4">
             <div>
               <h3 className="prose-xl mt-1.5">{activity.title}</h3>
-              <p>{activity.subtitle}</p>
+              <p className="my-1">{activity.subtitle}</p>
             </div>
             <div className="leading-normal prose-p:my-2 prose-ul:mt-0.5 prose-li:my-1">
               <MDXRemote
