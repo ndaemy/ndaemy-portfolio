94c25b9ea9c622bc307e5094398c76fc1278a345
ndaemy <yuyaebean@gmail.com>
2024-11-19 16:55:13 +0900

    feat: 프로젝트 상세 페이지 summary 부분 생성

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -20,10 +20,51 @@ export default async function Project({
   const project = getProject(slug);
 
   return (
-    <main className="prose flex flex-col items-center">
+    <main className="prose flex flex-col items-center gap-12">
       {project.images && (
         <Carousel imageUrls={project.images} className="not-prose h-96 w-3/4" />
       )}
+      <div className="flex w-full flex-col gap-4">
+        <h1 className="prose-2xl mb-0 text-4xl font-bold">{project.name}</h1>
+        <p className="prose-lg mt-0 text-xl text-base-content/80">
+          {project.description}
+        </p>
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center gap-2">
+            <span className="font-semibold">기간:</span>
+            <span>{project.startDate}</span>
+            <span>-</span>
+            {project.endDate ? (
+              <span>{project.endDate}</span>
+            ) : (
+              <span>Current</span>
+            )}
+          </div>
+          <div className="grid grid-cols-[max-content_1fr] gap-2">
+            <span className="font-semibold">역할:</span>
+            <div className="flex flex-wrap gap-x-2 gap-y-1">
+              {project.positions.map(position => (
+                <span key={position} className="badge badge-neutral my-1">
+                  {position}
+                </span>
+              ))}
+            </div>
+          </div>
+          {project.technologies && (
+            <div className="grid grid-cols-[max-content_1fr] gap-2">
+              <span className="font-semibold">사용 기술:</span>
+              <div className="flex flex-wrap gap-x-2 gap-y-1">
+                {project.technologies.map(tech => (
+                  <span key={tech} className="badge badge-neutral my-1">
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
       <MDXRemote source={project.content} />
     </main>
   );
