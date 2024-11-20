9df33ac952a75a9d0f5baf8d9059357bf91fd5a1
ndaemy <yuyaebean@gmail.com>
2024-11-21 04:28:29 +0900

    feat: 포트폴리오를 pdf 로 내보내는 스크립트 작성

---
+++ src/lib/export-cv.ts
@@ -0,0 +1,25 @@
+import puppeteer from "puppeteer";
+
+export const exportCV = async () => {
+  const productionOrigin = process.env.PRODUCTION_ORIGIN;
+
+  if (!productionOrigin) {
+    throw new Error("PRODUCTION_ORIGIN is not defined");
+  }
+
+  const browser = await puppeteer.launch({ browser: "firefox" });
+  const page = await browser.newPage();
+  await page.goto(productionOrigin, { waitUntil: "networkidle0" });
+  await page.pdf({
+    path: "dist/cv.pdf",
+    width: "1024px",
+    height: `${1024 * 1.414}px`,
+    margin: {
+      top: "1.2cm",
+      bottom: "1.2cm",
+    },
+  });
+  await browser.close();
+};
+
+exportCV();
