072506f87c55062b40a94499b8d4f71dc6c109c5
ndaemy <yuyaebean@gmail.com>
2024-11-24 21:18:03 +0900

    feat: AOS 와 iOS 용 manifests 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,8 +36,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { webAppTitle } = siteMetadata;
+
   return (
     <html lang="en" data-theme="dark">
+      <head>
+        <meta name="apple-mobile-web-app-title" content={webAppTitle} />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${pretendard.variable} antialiased`}>
         <div className="mx-10 flex flex-col items-center py-20 *:w-full *:max-w-screen-lg md:mx-14 print:pb-0 print:pt-10">
           {children}
--- src/resources/config.ts
+++ src/resources/config.ts
@@ -3,4 +3,5 @@ export const productionOrigin = process.env.PRODUCTION_ORIGIN;
 export const siteMetadata = {
   title: "유예빈 (Yu Yebin)",
   description: "유예빈 포트폴리오",
+  webAppTitle: "유예빈 포트폴리오",
 };
