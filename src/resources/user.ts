c1d05f25d1ae91499e06e19853d2a2a62371b0a8
ndaemy <yuyaebean@gmail.com>
2024-12-04 21:24:22 +0900

    feat: 전체 기술스택과 루미스케이프 기술스택에 Playwright 추가

--- src/resources/user.ts
+++ src/resources/user.ts
@@ -15,6 +15,7 @@ export const user = {
       "CSS-in-JS",
       "Tailwind CSS",
       "Storybook",
+      "Playwright",
     ],
     backend: ["Node.js", "Express", "Koa", "Strapi", "Prisma", "PostgreSQL"],
     etc: ["Git", "GitHub", "Vercel", "AWS", "Figma", "Google Analytics"],
